2ba332c5df9d84f96043b7d8b609027362adf015
crispian.batstone <cbatston@eld100.desktop.frd.metoffice.com>
2015-10-05 10:44:58 +0100

    Initial data

---
+++ read_PGE11_output.py
@@ -0,0 +1,10 @@
+# -*- coding: iso-8859-1 -*-
+
+file = ''
+
+# Define a list for cloud systems
+cloud_systems = []
+with open(...) as f:
+    for line in f:
+        if line[:2] == '==':
+          cloud_systems.append([]) # ?????
