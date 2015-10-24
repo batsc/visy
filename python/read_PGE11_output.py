07c98864a2c07dab82d1132404aae1f39eb473d2
batsc <crispianbatstone@yahoo.com>
2015-10-24 14:04:46 +0100

    Python to json

--- python/read_PGE11_output.py
+++ python/read_PGE11_output.py
@@ -1,4 +1,5 @@
 # -*- coding: iso-8859-1 -*-
+import json
 
 file = '../output/SAFNWC_MSG3_RDT__201507011515_ukamv_______.buf_section4'
 
@@ -6,7 +7,7 @@ class System(object):
     ''' Class to contain RDT information for a detected system
     '''
     def __init__(self, id, num_vertices):
-        self.uid = uid
+        self.uid = id
         self.num_vertices = num_vertices
         self.coords = []
     def add_coord(self, coords):
@@ -18,24 +19,43 @@ class CloudSystems(object):
     def __init__(self):
         self.systems = []
     def extract_region(self, lonmin, lonmax, latmin, latmax):
+        ''' Return a CloudSystem containing only systems within a defined
+            region
+        '''
+        pass
+    def to_json_file(self, filename):
+        ''' Output to JSON format
+        '''
+        json_dict = {}
+        for system in self.systems:
+            uid = "feature_" + "{:0>4d}".format(system.uid)
+            json_dict[uid] = {} 
+            json_dict[uid]['coords'] = system.coords
+        
+        with open(filename, "w") as text_file:
+            text_file.write(json.dumps(json_dict))
         
 # Define a list for cloud systems
-cs = CloudSystems()
+def read_RDT_section4(filename):
+    ''' Read text output of RDT, section 4
+    '''
+    cs = CloudSystems()
 
-uid = 0
-with open(file) as f:
-    for line in f:
-        # New system if num vertices specified
-        if line.find('Number of points of contour of the cloud system') > 0:
-            uid += 1
-            num_vertices = int(line.split(' ')[-1])
-            cs.systems.append(system(uid, num_vertices))
-            continue
-        # If these are coordinates, they alternate latitude, then longitude
-        if line.find('Latitude (coarse accuracy) of one point of contour') > 0:
-            lat_lon = [float(line.split(' ')[-1])]
-            continue
-        elif line.find('Longitude (coarse accuracy) of one point of contour') > 0:
-            lat_lon.append(float(line.split(' ')[-1]))
-            cloud_systems[-1].add_coord(lat_lon)
-            continue
+    uid = 0
+    with open(file) as f:
+        for line in f:
+            # New system if num vertices specified
+            if line.find('Number of points of contour of the cloud system') > 0:
+                uid += 1
+                num_vertices = int(line.split(' ')[-1])
+                cs.systems.append(System(uid, num_vertices))
+                continue
+            # If these are coordinates, they alternate latitude, then longitude
+            if line.find('Latitude (coarse accuracy) of one point of contour') > 0:
+                lat_lon = [float(line.split(' ')[-1])]
+                continue
+            elif line.find('Longitude (coarse accuracy) of one point of contour') > 0:
+                lat_lon.append(float(line.split(' ')[-1]))
+                cs.systems[-1].add_coord(lat_lon)
+                continue
+    return cs
