fa495a84d5f843ea477739c847823e70d5b60fd1
Crispian Batstone <batsc@linux-f01r.home>
2016-02-09 21:23:07 +0000

    mm

--- html/wind.js
+++ html/wind.js
@@ -1,18 +1,24 @@
 
+var img = new Image();
 
 $( document ).ready(function() {
   var ctx = $('#canvas')[0].getContext('2d');
   var img_src = "/home/batsc/Work/git/WorldWeatherSymbols/symbols/ddff_WindArrows/WeatherSymbol_WMO_WindArrowNH_47.svg";
 
-  var img = new Image();
+  var wd = 110;
+  var ht = 36;
   img.onload = function() {
-    drawRotatedImage(ctx, img, 10, 10, 45);
+    for (var j=0;j<20;j++) {
+      for (var i=0;i<9;i++) {
+        drawRotatedImage(ctx, img, (wd + j*wd)/2, ht + i*ht, (i + j*9) * 2, wd, ht);
+      }
+    }
   }
   img.src = img_src;
 });
 
 var TO_RADIANS = Math.PI/180;
-function drawRotatedImage(ctx, img, x, y, angle)
+function drawRotatedImage(ctx, img, x, y, angle, dheight, dwidth)
 {
     // save the current co-ordinate system
     // before we screw with it
@@ -27,7 +33,7 @@ function drawRotatedImage(ctx, img, x, y, angle)
 
     // draw it up and to the left by half the width
     // and height of the image
-    ctx.drawImage(img,  -(img.width/2), -(img.height/2));
+    ctx.drawImage(img,  -(dwidth/2), -(dheight/2), dwidth, dheight);
 
     // and restore the co-ords to how they were when we began
     ctx.restore();
--- rgb.js
+++ rgb.js
@@ -1,20 +1,45 @@
 
-$(document).ready(function() {
-  add_img($('#v1')[0], "images/vis06.png");
-  add_img($('#v2')[0], "images/vis08.png");
-  add_img($('#v3')[0], "images/vis16.png");
+var test_f = false;
 
-  var ctx, data;
+$(document).ready(function() {
+  add_img($('#v1')[0], "vis06_s.png", 0);
+  add_img($('#v2')[0], "vis08_s.png", 1);
+  add_img($('#v3')[0], "vis16_s.png", 2);
+
+  var ctx, data, ctxres;
   ctx = $('#v3')[0].getContext('2d');
   data = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+//   ctxres = $('#res')[0].getContext('2d');
+//   ctxres.canvas.width = ctx.canvas.width;
+//   ctxres.canvas.height = ctx.canvas.height;
+//   var numBytes = data.data.length;
+//   console.log(numBytes);
+//   var myImageData = ctxres.createImageData(ctx.canvas.width, ctx.canvas.height);
+//   ctxres.putImageData(data, 0, 0);
 });
 
-function add_img(canvas, filename) {
+function add_img(canvas, filename, rgb_index) {
   var img = new Image();
   img.onload = function(){
     canvas.width = img.width;
     canvas.height = img.height;
     canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
+    var d = canvas.getContext('2d').getImageData(0, 0, img.width, img.height);
+
+    if (test_f == false) {
+      $('#res')[0].getContext('2d').createImageData(d);
+      test_f = true; // MUST BE A BETTER WAY OF DOING THIS
+    }
+    var f = $('#res')[0].getContext('2d').getImageData(0, 0, img.width, img.height);
+//    f.data = f.data + d.data * rgb_index;
+    console.log(d.data);
+    for (var i=0; i<d.data.length; i++) {
+      f.data[i] = f.data[i] + d.data[i] * rgb_index;
+    }
+    console.log(f.data);
+    $('#res')[0].getContext('2d').putImageData(f, 0, 0);
+    var g = $('#res')[0].getContext('2d').getImageData(0, 0, img.width, img.height);
+//    console.log(g);
   }
   img.src = filename;
 }
